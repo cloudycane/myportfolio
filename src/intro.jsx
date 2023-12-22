20b9aa240e6dcc50e69a1b624fb29cee2c6310a6
Jay-Ar Abihay Tecson <jayarabihay@gmail.com>
2023-12-22 15:13:41 +0100

    changed intro.jsx

--- src/intro.jsx
+++ src/intro.jsx
@@ -5,9 +5,9 @@ export default function Intro(){
     return <div>
         
         <center>
-        <img src="./img/welcome.gif" />
-        <img src="./img/mejay.png" />
-        <img src="./img/mypic.jpg" style={{width:400, height:400, paddingTop:"5%"}} />
+        <img src="../img/welcome.gif" />
+        <img src="../img/mejay.png" />
+        <img src="../img/mypic.jpg" style={{width:400, height:400, paddingTop:"5%"}} />
         <br></br>
         </center>
         <h1>A visual art enthusiast, designer, and developer knowledgeable about programming languages for web development(front-end and back-end) and object oriented programming such as HTML5, CSS, JavaScript, and Python</h1>
